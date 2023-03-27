b58fb014d397aea1c05fe9231dc6e396be0c82b7
Donnell Reuben <donnell.reuben@icloud.com>
2023-03-27 14:17:33 -0500

    hard coded fortune teller in ruby, need to add to rails

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,11 +1,13 @@
 class MyExamplesController < ApplicationController
-  def myfortune
-    
-    render json: {message: "Hello, world!"}
   
+  def myfortune
+    render json: {message: "When fear hurts you, conquer it and defeat it!"}
+    
+    
   end 
 
-
+# Ruby code for this would be
+#
 
 
 
@@ -17,5 +19,11 @@ end
   
 Feature 1:
 The page should return a random fortune. Every time you refresh the page, you’ll get a new fortune. You should make up at least 3 different fortunes.
+
+Quotes:
+When fear hurts you, conquer it and defeat it!
+Keep your eye out for someone special.
+If you have something good in your life, don't let it go!
+
   
 =end
---
+++ fortune_radomizer.rb
@@ -0,0 +1,8 @@
+quotes = [
+"When fear hurts you, conquer it and defeat it!",
+"If you have something good in your life, don't let it go!", 
+"Keep your eye out for someone special in the future."
+
+]
+
+p quotes.sample
